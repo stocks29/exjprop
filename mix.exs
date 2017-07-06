189fb21c9a4e24d94608a0c1954a58cc98475714
Kyle A Anderson <kyanderson@zappos.com>
2017-07-06 10:56:44 -0400

    start logger

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Exjprop.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    []
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
